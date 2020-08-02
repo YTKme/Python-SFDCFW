8f89000ecaa46a39bf703901d6ff423d10404cf0
Yan Kuang <YTKme@Outlook.com>
2020-08-02 01:06:48 -0700

    Refactor TestRest module

--- SFDCAPI/Test/TestRest.py
+++ SFDCAPI/Test/TestRest.py
@@ -1,6 +1,6 @@
 """
-SFDCAPI.Test.Test
-~~~~~~~~~~~~~~~~~
+SFDCAPI.Test.TestRest
+~~~~~~~~~~~~~~~~~~~~~
 """
 
 import json
@@ -9,38 +9,58 @@ import unittest
 from SFDCAPI.Authentication.Access import Access
 from SFDCAPI.Rest.Rest import Rest
 from SFDCAPI.Constant.Constant import HTTP_GET
+from SFDCAPI.Constant.Constant import TEST_DATA
 
 
 class TestRest(unittest.TestCase):
-    """Test the Rest module."""
+    """Test the REST (REpresentational State Transfer) module."""
 
-    def test_send(self):
-        """Test a failure of the send method.
+    @classmethod
+    def setUpClass(cls):
+        """Prepare test set up class.
+
+        Get the Test Data from JSON (JavaScript Object Notation) file.
         """
 
         # Get the current directory of the file
         current_directory = os.path.dirname(os.path.abspath(__file__))
-        # Get the path of the test data file
-        test_access_file = os.path.join(current_directory, "Test_Access.json")
+        # Get the path of the Test Data file
+        cls.test_data_file = os.path.join(current_directory, TEST_DATA)
 
         # Open the file for reading
-        with open(test_access_file, "r") as f:
-            data = json.load(f)
+        with open(cls.test_data_file, 'r') as f:
+            cls.data = json.load(f)
 
-        # Get the user data for success login
-        user_success = data["user_success"]
+        # Get the URL (Uniform Resource Locator) from the Test Data
+        cls.url = cls.data['url']
 
-        # Create an instance of Access object and login
-        access = Access(username=user_success["username"], password=user_success["password"], domain="test", metadata=True).login()
+
+    def test_send_get_success(self):
+        """Test a success of the send method.
+        """
 
         # Create instance of Rest object
-        rest = Rest(access)
-        # Send a failure (invalid) GET request
-        response = rest.send(HTTP_GET, "/invalid", None)
+        rest = Rest()
+        # Send a GET request to a valid URL
+        response = rest.send(HTTP_GET, self.url)
 
-        # Test to ensure getting HTTP status code 200
+        # Test to ensure getting HTTP (HyperText Transfer Protocol) status code 200
         self.assertEqual(response.status_code, 200)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def suite():
+    """Test Suite"""
+
+    # Create the Unit Test Suite
+    suite = unittest.TestSuite()
+
+    # Add the Unit Test
+    suite.addTest(TestRest)
+
+    # Return the Test Suite
+    return suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
