4bbb76d3ffe7d3c79e279b53fd870602a059fa40
Yan Kuang <YTKme@Outlook.com>
2021-02-18 15:41:17 -0800

    Update setup.py information

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "SFDCAPI",
-    version = "0.1.0",
+    name = 'SFDCAPI',
+    version = '0.1.0',
     packages = find_packages(),
 
     # Dependency
     install_requires = [
-        "pandas",
-        "requests",
-        "zeep",
+        'pandas',
+        'requests',
+        'zeep',
     ],
 
     # Metadata
-    author = "Yan Kuang",
-    author_email = "YTKme@Outlook.com",
-    description = "Saleforce Application Programming Interface.",
-    license = "GNU GENERAL PUBLIC LICENSE",
-    keywords = "Salesforce"
+    author = 'Yan Kuang',
+    author_email = 'YTKme@Outlook.com',
+    description = 'Saleforce Application Programming Interface.',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+    keywords = 'Salesforce',
 )
