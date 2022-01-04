3b8d16134a5310c9ba011297168eb3a39c3d9836
Yan Kuang <YTKme@Outlook.com>
2022-01-04 10:39:21 -0800

    Update project setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the contents of the README.md file
+from pathlib import Path
+current_directory = Path(__file__).parent
+long_description = (current_directory/'README.md').read_text()
+
 setup(
-    name = 'SFDCAPI',
+    name = 'sfdcapi',
     version = '0.1.0',
     packages = find_packages(),
 
@@ -16,11 +21,13 @@ setup(
     author = 'Yan Kuang',
     author_email = 'YTKme@Outlook.com',
     description = 'Saleforce Application Programming Interface.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.5',
-    keywords = 'Salesforce',
+    keywords = 'salesforce sfdc api',
 )
