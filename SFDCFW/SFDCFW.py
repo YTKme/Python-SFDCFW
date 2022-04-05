e89628de12fdd04d1d5ffcf5811fe9cdcfbbb145
Yan Kuang <YTKme@Outlook.com>
2022-04-05 13:58:31 -0700

    Update SFDCFW module

--- SFDCFW/SFDCFW.py
+++ SFDCFW/SFDCFW.py
@@ -3,7 +3,7 @@ SFDCFW.SFDCFW
 ~~~~~~~~~~~~~
 """
 
-from urllib.parse import urlparse
+from __future__ import annotations
 
 from SFDCFW.Access import Access
 from SFDCFW.Rest import Rest
@@ -14,17 +14,19 @@ from SFDCFW.Constant import SFDC_API_V
 class SFDCFW:
     """Salesforce.com FrameWork."""
 
-    def __init__(self,
-                 username=None,
-                 password=None,
-                 security_token=None,
-                 client_id=None,
-                 client_secret=None,
-                 version=SFDC_API_V,
-                 domain='login',
-                 wsdl=None,
-                 metadata=False):
-        """Constructor.
+    def __init__(
+        self,
+        username: str = None,
+        password: str = None,
+        security_token: str = None,
+        client_id: str = None,
+        client_secret: str = None,
+        version: str = SFDC_API_V,
+        domain: str = 'login',
+        wsdl: str = None,
+        metadata: str = False
+    ) -> None:
+        """Instantiator.
 
         Args:
             username (str): The Salesforce user Username
@@ -37,84 +39,32 @@ class SFDCFW:
             wsdl (str): The path to the WSDL (Web Services Description Language) file
             metadata (bool): Whether or not this is for metadata
         """
-        pass
 
-        # # Create an instance of Access object and login
-        # access = Access(username=username,
-        #                 password=password,
-        #                 security_token=security_token,
-        #                 client_id=client_id,
-        #                 client_secret=client_secret,
-        #                 version=version,
-        #                 domain=domain,
-        #                 wsdl=wsdl,
-        #                 metadata=metadata).login()
-
-        # # Unpack the tuple for session ID / access token and server URL / instance URL
-        # id_token, base_url = access
-
-        # # Parse the URL
-        # u = urlparse(base_url)
-        # self.base_url = f'{u.scheme}://{u.netloc}'
-
-        # # Create REST header
-        # self.header = {
-        #     'Authorization': f'Bearer {id_token}',
-        #     'Content-Type': 'application/json; charset=utf-8',
-        #     'Accept': 'application/json'
-        # }
-
-
-    def __new__(cls,
-                username=None,
-                password=None,
-                security_token=None,
-                client_id=None,
-                client_secret=None,
-                version=SFDC_API_V,
-                domain='login',
-                wsdl=None,
-                metadata=False):
-        """Create a new instance of the `Rest` class and return it.
-
-        Args:
-            username (str): The Salesforce user Username
-            password (str): The Salesforce user Password
-            security_token (str): The Salesforce user Security Token
-            client_id (str): The Salesforce Connected App Consumer Key
-            client_secret (str): The Salesforce Connected App Consumer Secret
-            version (str): The Salesforce version of the Application Programming Interface
-            domain (str): The common Salesforce domain for connection (login or test)
-            wsdl (str): The path to the WSDL (Web Services Description Language) file
-            metadata (bool): Whether or not this is for metadata
-        """
+        # Set the name / label
+        self.label = None
 
         # Create an instance of Access object and login
-        access = Access(username=username,
-                        password=password,
-                        security_token=security_token,
-                        client_id=client_id,
-                        client_secret=client_secret,
-                        version=version,
-                        domain=domain,
-                        wsdl=wsdl,
-                        metadata=metadata).login()
-
-        # Create (`__new__`) and initialize (`__init__`) Rest object
-        rest = Rest(access)
-        # Return the Rest object
-        return rest
+        self.access = Access(username=username,
+                             password=password,
+                             security_token=security_token,
+                             client_id=client_id,
+                             client_secret=client_secret,
+                             version=version,
+                             domain=domain,
+                             wsdl=wsdl,
+                             metadata=metadata).login()
 
 
-    def __getattr__(self, label):
+    def __getattr__(self, label: str) -> SFDCFW:
         """Get Attribute Passed In.
 
         Args:
             label (str): The attribute passed in.
 
         Returns:
-            A instance of the SObject class.
+            A instance of the SFDCFW class.
         """
+
         # Set the name / label
         self.label = label
 
@@ -123,8 +73,27 @@ class SFDCFW:
 
 
     @property
-    def rest(self):
-        # Create a Rest object
-        rest = Rest(self.access)
+    def rest(self) -> Rest:
+        """Rest.
 
+        A Rest property for the REST (REpresentational State Transfer)
+        API (Application Programming Interface)
+
+        Returns:
+            A instance of the Rest class.
+        """
+
+        # Create and instantiate a Rest object
+        rest = Rest(self.access)
+        # Return the Rest object
         return rest
+
+    
+    def metadata(self):
+        """Metadata
+
+        A Metadata property for the Metadata API (Application
+        Programming Interface)
+        """
+
+        pass
