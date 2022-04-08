3358a27ec3c033dd4d00c336a4ea083f21a566da
Yan Kuang <YTKme@Outlook.com>
2022-04-08 13:05:28 -0700

    Update SFDCFW core

--- SFDCFW/SFDCFW.py
+++ SFDCFW/SFDCFW.py
@@ -7,12 +7,13 @@ from __future__ import annotations
 
 from SFDCFW.Access import Access
 from SFDCFW.Rest import Rest
+from SFDCFW.Metadata import Metadata
 
 from SFDCFW.Constant import SFDC_API_V
 
 
 class SFDCFW:
-    """Salesforce.com FrameWork."""
+    """Salesforce.com FrameWork"""
 
     def __init__(
         self,
@@ -23,25 +24,29 @@ class SFDCFW:
         client_secret: str = None,
         version: str = SFDC_API_V,
         domain: str = 'login',
-        wsdl: str = None,
-        metadata: str = False
+        wsdl: dict = None
     ) -> None:
         """Instantiator.
 
         Args:
-            username (str): The Salesforce user Username
-            password (str): The Salesforce user Password
-            security_token (str): The Salesforce user Security Token
-            client_id (str): The Salesforce Connected App Consumer Key
-            client_secret (str): The Salesforce Connected App Consumer Secret
-            version (str): The Salesforce version of the Application Programming Interface
-            domain (str): The common Salesforce domain for connection (login or test)
-            wsdl (str): The path to the WSDL (Web Services Description Language) file
-            metadata (bool): Whether or not this is for metadata
+            username (str): The Salesforce user Username.
+            password (str): The Salesforce user Password.
+            security_token (str): The Salesforce user Security Token.
+            client_id (str): The Salesforce Connected App Consumer Key.
+            client_secret (str): The Salesforce Connected App Consumer Secret.
+            version (str): The Salesforce version of the Application Programming Interface.
+            domain (str): The common Salesforce domain for connection (login or test).
+            wsdl (dict): The path(s) to the WSDL (Web Services Description Language) file(s).
         """
 
         # Set the name / label
         self.label = None
+
+        # Get the WSDL
+        self.wsdl = wsdl
+
+        # Parse the enterprise WSDL
+        enterprise_wsdl = self.wsdl['enterprise']
 
         # Create an instance of Access object and login
         self.access = Access(username=username,
@@ -51,8 +56,7 @@ class SFDCFW:
                              client_secret=client_secret,
                              version=version,
                              domain=domain,
-                             wsdl=wsdl,
-                             metadata=metadata).login()
+                             wsdl=enterprise_wsdl).login()
 
 
     def __getattr__(self, label: str) -> SFDCFW:
@@ -74,10 +78,10 @@ class SFDCFW:
 
     @property
     def rest(self) -> Rest:
-        """Rest.
+        """Rest
 
         A Rest property for the REST (REpresentational State Transfer)
-        API (Application Programming Interface)
+        API (Application Programming Interface).
 
         Returns:
             A instance of the Rest class.
@@ -85,15 +89,24 @@ class SFDCFW:
 
         # Create and instantiate a Rest object
         rest = Rest(self.access)
+
         # Return the Rest object
         return rest
 
     
+    @property
     def metadata(self):
-        """Metadata
+        """Metadata.
 
         A Metadata property for the Metadata API (Application
-        Programming Interface)
+        Programming Interface).
         """
 
-        pass
+        # Parse the metadata WSDL
+        metadata_wsdl = self.wsdl['metadata']
+
+        # Create and instantiate a Metadata object
+        metadata = Metadata(self.access, metadata_wsdl)
+
+        # Return the Metadata object
+        return metadata
